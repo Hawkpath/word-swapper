fbe0ee6ccbd1ab91c807e87904563f6543553f17
Hawkpath <hawkpathas@gmail.com>
2021-05-15 19:49:20 -0400

    Use a preexisting language model to find similar words.

--- musician_pun/__init__.py
+++ musician_pun/__init__.py
@@ -1,84 +1,81 @@
 import random
-from typing import Annotated as A, Set, Tuple, TypeVar
+from typing import List, Optional, Tuple, TypeVar, cast
 
+import gensim.downloader as gensim_api
+from gensim.models.keyedvectors import Word2VecKeyedVectors
 from nltk import word_tokenize
-from nltk.corpus import wordnet, words as corpus_words
+from nltk.corpus import words as corpus_words
 from nltk.tokenize import LegalitySyllableTokenizer
+
+__all__ = (
+    'make_pun',
+)
 
 V = TypeVar('V')
 
+model: Word2VecKeyedVectors = gensim_api.load("glove-twitter-25")
 
-class DerivativeWord:
+
+class SubwordFinder:
+    """Find subwords within an existing word"""
 
     __tokenizer = LegalitySyllableTokenizer(corpus_words.words())
 
     def __init__(self, word: str):
         self.word = word
-        self.syllables = self.__tokenizer.tokenize(word)
-        self._generate_derivatives()
+        self.syllables: List[str] = self.__tokenizer.tokenize(word)
+        self._generate_splits()
 
-    def _generate_derivatives(self):
+    def _generate_splits(self):
         sylls = self.syllables
-        self.derivatives = []
+        self.subwords = []
         for win_len in range(len(sylls), 0, -1):
             for offset in range(len(sylls) - win_len + 1):
-                subword = ''.join(sylls[offset:offset+win_len])
-                _, antonyms = self.thesaurize(subword)
-                for antonym in antonyms:
-                    self.derivatives.append(''.join((
-                        sylls[:offset] + [antonym] + sylls[offset+win_len:]
-                    )))
+                subword = ''.join(sylls[offset:offset+win_len]).lower()
+                if not subword in model:
+                    continue
+                self.subwords.append((
+                    sylls[:offset], subword, sylls[offset+win_len:]
+                ))
 
     def __len__(self):
-        return len(self.derivatives)
+        return len(self.subwords)
 
     def __iter__(self):
-        return iter(self.derivatives)
+        return iter(self.subwords)
 
     def __getitem__(self, index: int):
-        return self.derivatives[index]
-
-    @staticmethod
-    def thesaurize(
-            word: str
-    ) -> Tuple[A[Set[str], 'synonyms'], A[Set[str], 'antonyms']]:
-        synonyms = []
-        antonyms = []
-
-        for syn in wordnet.synsets(word):
-            for lemma in syn.lemmas():
-                synonyms.append(lemma.name())
-                if lemma.antonyms():
-                    antonyms.append(lemma.antonyms()[0].name())
-
-        return set(synonyms), set(antonyms)
+        return self.subwords[index]
 
 
-def make_pun(text: str):
+def make_pun(text: str, similar_count=10) -> Optional[str]:
     text = text.replace('-', ' ')
-    words = []
-    derivatives_count = 0
-    for word in word_tokenize(text):
-        derivative = DerivativeWord(word)
-        words.append(derivative)
-        derivatives_count += len(derivative)
+    words = [SubwordFinder(w) for w in word_tokenize(text)]
 
-    if derivatives_count:
-        choice = random.randint(0, derivatives_count-1)
-    else:
-        choice = 0
-    out = []
-    for word in words:
-        try:
-            out.append(word[choice])
-            choice = None
-        except (IndexError, TypeError):
-            out.append(word.word)
-            if choice is not None:
-                choice -= len(word)
+    out = [w.word for w in words]
 
+    splitted = [w for w in words if w.subwords]
+    if not splitted:
+        return
+    # Get a random splittable word
+    i, subfinder = random.choice(list(enumerate(splitted)))
+    subfinder = cast(SubwordFinder, subfinder)
+
+    # Get a random subword
+    subword_start, subword, subword_end = random.choice(subfinder.subwords)
+
+    # Get a list of similar words from the model
+    similars: List[Tuple[str, float]] = model.most_similar(
+        positive=subword, topn=similar_count
+    )
+    # Pick a random similar word using their weights
+    changed = random.choices(
+        population=[i[0] for i in similars],
+        weights=[i[1] for i in similars],
+        k=1
+    )[0]
+    # Join the splits of this word together with the random similar word
+    changed = ''.join(subword_start + [changed] + subword_end)
+
+    out[i] = changed
     return ' '.join(out)
-
-
-a = make_pun('Third Eye Blind')
-pass
