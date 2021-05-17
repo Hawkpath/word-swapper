efeb6040da1bd2f35b725eb3c63974129b56e7ec
Hawkpath <hawkpathas@gmail.com>
2021-05-16 22:20:10 -0400

    Use coloredlogs for colored console logging.

--- musician_pun/__init__.py
+++ musician_pun/__init__.py
@@ -1,13 +1,12 @@
 import logging
-import sys
+
+import coloredlogs
 
 logger = logging.getLogger('musician_pun')
-
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(sys.stdout)
-handler.setFormatter(logging.Formatter(
-    '%(asctime)s|%(levelname)s|%(name)s|%(message)s'
-))
-logger.addHandler(handler)
+coloredlogs.DEFAULT_FIELD_STYLES['name'] = {'color': 'white', 'faint': True}
+coloredlogs.install(
+    level='DEBUG', logger=logger,
+    fmt='%(asctime)s %(name)s %(levelname)s %(message)s'
+)
 
 from .bot import Bot, main
